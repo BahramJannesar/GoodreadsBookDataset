6445179bb22ee34ed14b2bf80ee85ab81d11f585
Bahram <ba.jannesar@gmail.com>
2020-06-26 05:10:43 +0430

    bug fix on twice add first page of reviews

--- review_scrapper.py
+++ review_scrapper.py
@@ -49,30 +49,43 @@ elif (counts_of_text_review / 30) <= 1 and (counts_of_text_review / 30) > 0 :
 
 print(limit_of_page_review)
 
-i = 1
+i = 0
 reviwe_list = []
 if counts_of_text_review != 0:
-    while i <= limit_of_page_review:
+    while i < limit_of_page_review:
+        # spolier_reviews = driver.find_elements_by_css_selector('em a')
+        # for spoiler_button in spolier_reviews:
+        #     spoiler_button.click()
 
+        # read_more_button = driver.find('#bookReviews .readable:nth-child(1) > a')
+        # for read_button in read_more_button:
+        #     read_button.click()
+
+        
         reviews = driver.find_elements_by_xpath(
             '//*[(@id = "bookReviews")]//*[contains(concat( " ", @class, " " ), concat( " ", "readable", " " ))]//span')
 
         for each in reviews:
             print(each.text)
             reviwe_list.append(each.text)
-        time.sleep(4)
-        button_next_page = driver.find_element_by_xpath(
-            '//*[contains(concat( " ", @class, " " ), concat( " ", "next_page", " " ))]')
-        button_next_page.click()
-        i = 1 + i
+
+ 
+        if limit_of_page_review > 1:
+            time.sleep(4)
+            button_next_page = driver.find_element_by_xpath(
+                '//*[contains(concat( " ", @class, " " ), concat( " ", "next_page", " " ))]')
+            button_next_page.click()
+            i = 1 + i
+        else:
+            break
 
     book_dic = {
                     "Id": int(book_id),
-                    "BookName": book_name,
-                    "reviews": reviwe_list
+                    "BookName": book_name.text,
+                    "reviews": reviwe_list[62:-1]
 
-                }
-
+    }
+    
     book_js = json.dumps(book_dic, indent=4)
     with open('book.json', 'w') as file:
         file.write(book_js)
