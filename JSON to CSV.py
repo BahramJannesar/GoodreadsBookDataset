e0fd24589f7452e42cbd2d129b59a44795b6f785
Bahram <ba.jannesar@gmail.com>
2020-07-22 17:28:06 +0430

    some bug fix

--- JSON to CSV.py
+++ JSON to CSV.py
@@ -2,11 +2,7 @@ import pandas as pd
 import glob
 import os
 
-os.chdir('JSON datasets')
-os.mkdir('CSV datasets')
-json_globe = glob.glob('*.json')
-for item in json_globe:
-    df = pd.read_json(item)
-    item = item.replace('.json' , '')
-    df.to_csv('CSV datasets/{}.csv'.format(item) , index = False , encoding = 'utf-8')
-    print(item + ' Done!')
+
+df = pd.read_json('book1700k-1800k.json')
+df.to_csv('book1700k-1800k.csv' , index = False , encoding = 'utf-8')
+
