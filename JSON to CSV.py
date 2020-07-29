e47137c775f26de5c7403c6c3ac7109455bdcede
Bahram <ba.jannesar@gmail.com>
2020-07-29 10:48:20 +0430

    added new dataset

--- JSON to CSV.py
+++ JSON to CSV.py
@@ -3,6 +3,6 @@ import glob
 import os
 
 
-df = pd.read_json('book1700k-1800k.json')
-df.to_csv('book1700k-1800k.csv' , index = False , encoding = 'utf-8')
+df = pd.read_json('book1800k-1900k.json')
+df.to_csv('book1800k-1900k.csv' , index = False , encoding = 'utf-8')
 
