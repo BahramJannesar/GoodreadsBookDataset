d6e69db2ebe67ad05cc8349c300e6493cf83035e
Bahram <ba.jannesar@gmail.com>
2020-09-03 15:10:25 +0430

    some changes

--- JSON to CSV.py
+++ JSON to CSV.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import glob
-import os
 
-
-df = pd.read_json('book1800k-1900k.json')
-df.to_csv('book1800k-1900k.csv' , index = False , encoding = 'utf-8')
+df = pd.read_json('book1900k-2000k.json')
+df.to_csv('book1900k-2000k.csv' , index = False , encoding = 'utf-8')
 
